e93f613ca12e64159827bcb68434a0bd20d7ac1e
[cristianbovenzi]git config --global user.name [cristianbovenzi] <[cristianbovenzi@gmail.com]>
2025-03-26 16:23:29 +0100

    Aggiunto altro metodo

--- Node.js/sum.js
+++ Node.js/sum.js
@@ -1,6 +1,12 @@
 const somma = 5 + 6;
 console.log("Il totale è:", somma);
 
+//* CON FUNZIONE
+function somma(a, b) {
+  return a + b;
+}
+console.log("Il totale è:", somma(5, 6));
+
 //TERMINALE
 //* $ node sum.js
 //* Il totale è: 11
